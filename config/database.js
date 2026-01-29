8a2f8caa9408b80b3a29b4e74175754fdda3da61
Zeeshan-kh4n <zeeshankhan@digitalocean.com>
2026-01-29 08:31:13 +0500

    Refactor database configuration to use connectionString

--- config/database.js
+++ config/database.js
@@ -2,18 +2,9 @@ module.exports = ({ env }) => ({
   connection: {
     client: 'postgres',
     connection: {
-      host: env('DATABASE_HOST'),
-      port: env.int('DATABASE_PORT', 5432),
-      database: env('DATABASE_NAME'),
-      user: env('DATABASE_USERNAME'),
-      password: env('DATABASE_PASSWORD'),
-      ssl: env.bool('DATABASE_SSL', true)
-        ? { rejectUnauthorized: false }
-        : false,
+      connectionString: env('DATABASE_URL'),
+      ssl: { rejectUnauthorized: false }
     },
-    pool: {
-      min: 0,
-      max: 5,
-    },
-  },
+    pool: { min: 2, max: 10 }
+  }
 });
