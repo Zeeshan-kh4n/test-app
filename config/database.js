ecdad50aa236b57f3340b6ce15238d277c711178
Zeeshan-kh4n <zeeshankhan@digitalocean.com>
2026-01-29 08:13:15 +0500

    Change database client from SQLite to Postgres

--- config/database.js
+++ config/database.js
@@ -1,10 +1,19 @@
-// config/database.js
 module.exports = ({ env }) => ({
   connection: {
-    client: 'sqlite',
+    client: 'postgres',
     connection: {
-      filename: env('DATABASE_FILENAME', '.tmp/data.db'),
+      host: env('DATABASE_HOST'),
+      port: env.int('DATABASE_PORT', 5432),
+      database: env('DATABASE_NAME'),
+      user: env('DATABASE_USERNAME'),
+      password: env('DATABASE_PASSWORD'),
+      ssl: env.bool('DATABASE_SSL', true)
+        ? { rejectUnauthorized: false }
+        : false,
     },
-    useNullAsDefault: true,
+    pool: {
+      min: 0,
+      max: 5,
+    },
   },
 });
